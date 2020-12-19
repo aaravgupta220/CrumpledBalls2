1b7caaeabc1c3deb11c928d293d1a81f3567d2a3
aaravgupta220 <73989010+aaravgupta220@users.noreply.github.com>
2020-12-20 00:33:20 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,30 @@
+class Paper {
+
+    constructor(x, y, radius){
+
+        var options = {
+            isStatic : false,
+            restitution : 0.3,
+            friction : 0.5,
+            density : 1.2
+        }
+
+        this.body = Bodies.circle(x, y, radius/2, options);
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.image = loadImage("paper.png");
+        World.add(world, this.body);
+    }
+        display (){
+            var pos = this.body.position;
+            push();
+            translate(pos.x,pos.y);
+            fill("grey");
+            ellipse(0, 0, this.radius);
+            imageMode(CENTER);
+            image(this.image, 0, 0, this.radius*2, this.height = this.radius*3);
+            pop();
+        }
+
+}
